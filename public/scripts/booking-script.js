7c2b3bcc0b2092f59c25c49183f3bd11ba6e92c8
Guillaume Béhague <guilitch7@msn.com>
2023-09-22 19:14:11 +0200

    fix book sript

--- public/scripts/booking-script.js
+++ public/scripts/booking-script.js
@@ -139,7 +139,7 @@ function object() {
     
     // appel à l'api
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://young-coast-00769.herokuapp.com/api/api-liste');
+    xhr.open('GET', 'http://young-coast-00769.herokuapp.com/api/api-liste');
     xhr.send();
   
     return new Promise((resolve, reject) => {
